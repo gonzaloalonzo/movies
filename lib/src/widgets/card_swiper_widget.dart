b4c23c7ca06b3c6b88dae93fc2d6dd2ab93365cd
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-11 22:13:16 -0400

    diagramacion de paginas

---
+++ lib/src/widgets/card_swiper_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({Key key}) : super(key: key);
+
+  final List<dynamic> peliculas;
+
+  CardSwiper({ @required this.peliculas });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(top:10.0),
+          child: Swiper(
+            layout:SwiperLayout.STACK,
+            itemWidth: _screenSize.width * 0.7,//width del 70% de la pantalla
+            itemHeight: _screenSize.height * 0.5,//height 50% de la pantalla
+            itemBuilder: (BuildContext context, int index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child:Image.network("http://via.placeholder.com/350x150",
+                fit: BoxFit.cover)
+              );
+              
+          },
+            itemCount: peliculas.length,
+            //pagination: SwiperPagination(),// puntos que muestra al final de la imagen
+            //control: SwiperControl(),// flechas que permiten el swip.
+        ),
+        );
+  }
+}
